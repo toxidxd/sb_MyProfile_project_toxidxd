20e71076602f26ed83483ff482f8d4a2d1588b9e
toxidxd <toxid13@yandex.ru>
2022-11-29 01:38:32 +0300

    rename variables

--- task.py
+++ task.py
@@ -7,30 +7,33 @@ name = ''
 age = 0
 phone = ''
 email = ''
-i = ''
+info = ''
 # social links
 vk = ''
-twitter = ''
+telegram = ''
 tiktok = ''
 
 
-def general_info_user(n_parameter, a_parameter, ph_parameter, e_parameter, i_parameter):
+def general_info_user(name_parameter, age_parameter, phone_parameter, email_parameter, info_parameter):
     print(SEPARATOR)
-    print('Имя:    ', n_parameter)
-    if 11 <= a_parameter % 100 <= 19: years_parameter = 'лет'
-    elif a_parameter % 10 == 1: years_parameter = 'год'
-    elif 2 <= a_parameter % 10 <= 4: years_parameter = 'года'
-    else: years_parameter = 'лет'
+    print('Имя:    ', name_parameter)
+    if 11 <= age_parameter % 100 <= 19:
+        years_parameter = 'лет'
+    elif age_parameter % 10 == 1:
+        years_parameter = 'год'
+    elif 2 <= age_parameter % 10 <= 4:
+        years_parameter = 'года'
+    else:
+        years_parameter = 'лет'
 
+    print('Возраст:', age_parameter, years_parameter)
+    print('Телефон:', phone_parameter)
+    print('E-mail: ', email_parameter)
+    if info:
+        print('')
+        print('Дополнительная информация:')
+        print(info_parameter)
 
-    print('Возраст:', a_parameter, years_parameter)
-    print('Телефон:', ph_parameter)
-    print('E-mail: ', e_parameter)
-    if i:
-            print('')
-            print('Дополнительная информация:')
-            print(i_parameter)
-    
 
 print('Приложение MyProfile')
 print('Сохраняй информацию о себе и выводи ее в разных форматах')
@@ -45,7 +48,7 @@ while True:
 
     option = int(input('Введите номер пункта меню: '))
     if option == 0:
-            break
+        break
 
     elif option == 1:
         # submenu 1: edit info
@@ -59,33 +62,33 @@ while True:
             option2 = int(input('Введите номер пункта меню: '))
             if option2 == 0:
                 break
-            
+
             elif option2 == 1:
                 # input general info
                 name = input('Введите имя: ')
                 while 1:
-                        # validate user age
-                        age = int(input('Введите возраст: '))
-                        if age > 0:
-                            break
-                        print('Возраст должен быть положительным')
+                    # validate user age
+                    age = int(input('Введите возраст: '))
+                    if age > 0:
+                        break
+                    print('Возраст должен быть положительным')
 
                 uph = input('Введите номер телефона (+7ХХХХХХХХХХ): ')
-                ph = ''
+                phone = ''
                 for ch in uph:
                     if ch == '+' or ('0' <= ch <= '9'):
-                        ph += ch
+                        phone += ch
 
-
-                e = input('Введите адрес электронной почты: ')
-                i = input('Введите дополнительную информацию:\n')
+                email = input('Введите адрес электронной почты: ')
+                info = input('Введите дополнительную информацию:\n')
 
             elif option2 == 2:
                 # input social links
-                v = input('Введите адрес профиля Вконтакте: ')
-                t = input('Введите логин Telegram: ')
-                tk = input('Введите логин Tiktok: ')
-            else: print('Введите корректный пункт меню')
+                vk = input('Введите адрес профиля Вконтакте: ')
+                telegram = input('Введите логин Telegram: ')
+                tiktok = input('Введите логин Tiktok: ')
+            else:
+                print('Введите корректный пункт меню')
 
     elif option == 2:
         # submenu 2: print info
@@ -99,19 +102,21 @@ while True:
             option2 = int(input('Введите номер пункта меню: '))
             if option2 == 0:
                 break
-            
+
             elif option2 == 1:
-                general_info_user(name, age, ph, e, i)
+                general_info_user(name, age, phone, email, info)
 
             elif option2 == 2:
-                general_info_user(name, age, ph, e, i)
+                general_info_user(name, age, phone, email, info)
 
                 # print social links
                 print('')
                 print('Социальные сети и мессенджеры')
-                print('Вконтакте:', v)
-                print('Telegram: ', t)
-                print('Tiktok:   ', tk)
-            else:   print('Введите корректный пункт меню')
+                print('Вконтакте:', vk)
+                print('Telegram: ', telegram)
+                print('Tiktok:   ', tiktok)
+            else:
+                print('Введите корректный пункт меню')
 
-    else:       print('Введите корректный пункт меню')
+    else:
+        print('Введите корректный пункт меню')
